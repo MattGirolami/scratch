405c3e2bf8ff820f11c8a25fe058d7c4131d98ec
MattGirolami <MattGirolami@users.noreply.github.com>
2019-05-15 22:13:46 -0600

    prettier spacing on get lambda code

--- notes-app-api/get.js
+++ notes-app-api/get.js
@@ -19,7 +19,7 @@ export async function main(event, context) {
       //Return the retreieved item
       return success(result.Item)
     } else {
-      return failure({status: false, error: "Item not found."});
+      return failure({ status: false, error: "Item not found." });
     }
   } catch (e) {
     return failure({ status: false });
